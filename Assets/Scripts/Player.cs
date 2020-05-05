3673cde8956d583747f7985fb570bee7d179e93e
AronT <aron.thompson@racunarstvo.hr>
2020-05-05 21:40:54 +0200

    E01 Set-up

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    Controller2D controller;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+
+
+}
