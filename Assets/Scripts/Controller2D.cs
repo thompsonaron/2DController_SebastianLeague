a8c7bac45752bc743fb53da67418cd60194e118c
AronT <aron.thompson@racunarstvo.hr>
2020-05-06 08:38:16 +0200

    Ep02 Collisions

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Controller2D : MonoBehaviour
 {
+    public LayerMask collisionMask;
+
     const float skinWidth = .015f;
     public int horizontalRayCount  = 4;
     public int verticalRayCount  = 4;
@@ -19,16 +21,72 @@ public class Controller2D : MonoBehaviour
     private void Start()
     {
         collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+        // prevents "bouncing"
+        Physics2D.autoSyncTransforms = true;
     }
 
-    private void Update()
+    public void Move(Vector3 moveDistance)
     {
         UpdateRaycastOrigins();
-        CalculateRaySpacing();
+
+        if (moveDistance.x != 0)
+        {
+            HorizontalCollisions(ref moveDistance);
+        }
+        
+        if (moveDistance.y != 0)
+        {
+            VerticalCollisions(ref moveDistance);
+        }
+        transform.Translate(moveDistance);
+    }
+
+    private void HorizontalCollisions(ref Vector3 velocity)
+    {
+        // Mathf.Sign returns -1 if going down, 1 if going up
+        float directionX = Mathf.Sign(velocity.x);
+        // how far away from collision 
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            // is player moving left? start raycasting from bottom left : start raycasting from bottom right
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i );
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+            
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+    private void VerticalCollisions(ref Vector3 velocity)
+    {
+        // Mathf.Sign returns -1 if going down, 1 if going up
+        float directionY = Mathf.Sign(velocity.y);
+        // how far away from collision 
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
 
         for (int i = 0; i < verticalRayCount; i++)
         {
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+            // is player moving down? start raycasting from bottom left : start raycasting from top left
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+            }
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
+    float moveSpeed = 6;
+    float gravity = -20f;
+    Vector3 velocity;
+
     Controller2D controller;
 
     void Start()
@@ -12,5 +16,13 @@ public class Player : MonoBehaviour
         controller = GetComponent<Controller2D>();
     }
 
+    private void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 
 }
